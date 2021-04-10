eae2c9875bac11b985d309d0a932a959aa494e2a
tayranesma <75939647+tayranesma@users.noreply.github.com>
2021-04-10 21:32:44 +0300

    Update hw-3.py

--- Homeworks/hw-3.py
+++ Homeworks/hw-3.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  7 22:22:31 2021
 
-@author: LENOVO
-Homework-3
-"""
-#%%
 """
 User login application:
     get username and password values from the user.
@@ -26,7 +20,7 @@ elif (username != get_username and password != get_password):
     print("Both username and password are wrong, please try again!")
 else:
     print("Congrats! :)")
-#%%
+
 """
    Extra: try building the same user login application but this time use a dictionary. 
 """
